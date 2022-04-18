5eea96224d2731addfe83ffec7d21476adef670f
tumuyan <tumuyan@gmail.com>
2022-04-18 10:36:06 +0800

    fix: keyboard modifer not be used

--- app/src/main/java/com/osfans/trime/ime/core/Trime.java
+++ app/src/main/java/com/osfans/trime/ime/core/Trime.java
@@ -864,14 +864,14 @@ public class Trime extends LifecycleInputMethodService {
 
   @Override
   public boolean onKeyDown(int keyCode, KeyEvent event) {
-    Timber.i("onKeyDown = %s", event);
+    Timber.i("\t<TrimeInput>\tonKeyDown()\tkeycode=%d, event=%s", keyCode, event);
     if (composeEvent(event) && onKeyEvent(event)) return true;
     return super.onKeyDown(keyCode, event);
   }
 
   @Override
   public boolean onKeyUp(int keyCode, KeyEvent event) {
-    Timber.i("onKeyUp = %s", event);
+    Timber.i("\t<TrimeInput>\tonKeyUp()\tkeycode=%d, event=%s", keyCode, event);
     if (composeEvent(event) && textInputManager.getNeedSendUpRimeKey()) {
       textInputManager.onRelease(keyCode);
       return true;
@@ -885,8 +885,9 @@ public class Trime extends LifecycleInputMethodService {
    * @param event {@link KeyEvent 按鍵事件}
    * @return 是否成功處理
    */
+  // KeyEvent 处理实体键盘事件
   private boolean onKeyEvent(@NonNull KeyEvent event) {
-    Timber.i("onKeyEvent = %s", event);
+    Timber.i("\t<TrimeInput>\tonKeyEvent()\tRealKeyboard event=%s", event);
     int keyCode = event.getKeyCode();
     textInputManager.setNeedSendUpRimeKey(Rime.isComposing());
     if (!isComposing()) {
@@ -954,17 +955,28 @@ public class Trime extends LifecycleInputMethodService {
     }
   }
 
+  // 处理键盘事件(Android keycode)
   public boolean handleKey(int keyEventCode, int metaState) { // 軟鍵盤
     textInputManager.setNeedSendUpRimeKey(false);
     if (onRimeKey(Event.getRimeEvent(keyEventCode, metaState))) {
+      // 如果输入法消费了按键事件，则需要释放按键
       textInputManager.setNeedSendUpRimeKey(true);
-      Timber.i("Rime onKey");
+      Timber.d(
+          "\t<TrimeInput>\thandleKey()\trimeProcess, keycode=%d, metaState=%d",
+          keyEventCode, metaState);
     } else if (performEnter(keyEventCode) || handleBack(keyEventCode)) {
       Timber.i("Trime onKey");
+      // 处理返回键（隐藏软键盘）和回车键（换行）
+      // todo 确认是否有必要单独处理回车键？是否需要把back和escape全部占用？
+      Timber.d("\t<TrimeInput>\thandleKey()\tEnterOrHide, keycode=%d", keyEventCode);
     } else if (ShortcutUtils.INSTANCE.openCategory(keyEventCode)) {
-      Timber.i("Open category");
+      // 打开系统默认应用
+      Timber.d("\t<TrimeInput>\thandleKey()\topenCategory keycode=%d", keyEventCode);
     } else {
       textInputManager.setNeedSendUpRimeKey(true);
+      Timber.d(
+          "\t<TrimeInput>\thandleKey()\treturn FALSE, keycode=%d, metaState=%d",
+          keyEventCode, metaState);
       return false;
     }
     return true;
--- app/src/main/java/com/osfans/trime/util/DialogUtils.kt
+++ app/src/main/java/com/osfans/trime/util/DialogUtils.kt
@@ -2,9 +2,6 @@ package com.osfans.trime.util
 
 import android.app.ProgressDialog
 import android.content.Context
-import android.view.LayoutInflater
-import androidx.appcompat.app.AlertDialog
-import com.osfans.trime.databinding.DialogLoadingBinding
 
 fun createLoadingDialog(context: Context, textId: Int): ProgressDialog {
     @Suppress("DEPRECATION")
